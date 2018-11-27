f392e190ede43acc7c921ba51e3a5018cf184161
lichunyang <licy@ibobei.cn>
2018-11-27 18:13:25 +0800

    新增 固有尺寸控件尺寸处理

--- CSS/Examples/Example/Example7Controller.m
+++ CSS/Examples/Example/Example7Controller.m
@@ -7,6 +7,9 @@
 //
 
 #import "Example7Controller.h"
+#import <MineCommonUtils/MineCommonUtils.h>
+#import <YogaKit/UIView+Yoga.h>
+#import "UIColor+Random.h"
 
 @interface Example7Controller ()
 
@@ -18,5 +21,30 @@
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor whiteColor];
+    
+    UIEdgeInsets edge = controllerSafeInset(SafeAreaScopeNavigationBar, nil);
+    
+    [self.view configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+        layout.isEnabled = YES;
+        layout.paddingTop = YGPointValue(edge.top);
+        layout.paddingLeft = YGPointValue(edge.left);
+        layout.paddingBottom = YGPointValue(edge.bottom);
+        layout.paddingRight = YGPointValue(edge.right);
+        layout.justifyContent = YGJustifyCenter;
+        layout.alignItems = YGAlignCenter;
+    }];
+    
+    
+    UILabel * l = [[UILabel alloc] initWithFrame:CGRectZero];
+    l.text = @"dfafafadfad";
+    l.textAlignment = NSTextAlignmentCenter;
+    l.backgroundColor = [UIColor redColor];
+    [self.view addSubview:l];
+    
+    [l configureLayoutWithBlock:^(YGLayout * _Nonnull layout) {
+        layout.isEnabled = YES;
+        layout.borderWidth = 10;
+    }];
+    [self.view.yoga applyLayoutPreservingOrigin:YES];
 }
 @end
